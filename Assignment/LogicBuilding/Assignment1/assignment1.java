f3bca0256045334b70dc83b2c16ea2ffbb82e3a7
patilkalyani03 <kalyanipatil0322@gmail.com>
2024-08-26 22:00:52 +0530

    Add files via upload

---
+++ Assignment/LogicBuilding/Assignment1/assignment1.java
@@ -0,0 +1,82 @@
+//Even or Odd NO:
+class main{
+    public static void main(String args[])
+    {
+        int i=20;
+        if(i%2==0)
+        System.out.println("number is even");
+        else
+        System.out.println("number is odd");
+    }
+}
+//Op:
+//Number is even
+//Positive or negative no:
+class main{
+    public static void main(String args[])
+    {
+        int i=10;
+        if(i>0)
+        System.out.println("number is positive");
+        else
+        System.out.println("number is negative");
+    }
+}
+//Op:
+//Number is positive
+
+//Area Of Square:
+class main{
+    public static void main(String args[])
+    {
+        int i=12;
+        int area=i*i;
+        System.out.println("Area of a square is: "+area);
+    }
+}
+//OP
+//Area of a square is: 144
+
+//Area of rectangle
+class main{
+    public static void main(String args[])
+    {
+        int l=12;
+        int w=11;
+        int area=l*w;
+        System.out.println("Area of a rectangle is: "+area);
+    }
+}
+//Op
+//Area of a rectangle is: 132
+
+//Find the largest of three number:
+class main{
+    public static void main(String args[])
+    {
+        int a=22,b=1,c=3;
+        if(a>b&&a>c){
+        System.out.println("largest number among three is  "+a);
+        }
+        else if(b>a&&b>c){
+             System.out.println("largest number among three is  "+b);
+        }
+        else 
+        System.out.println("largest number among three is  "+c);
+         
+    } 
+        }
+//Op
+//largest number among three is  22
+
+class assignment1{
+    public static void main(String args[])
+    {
+        int a = 10;
+        if(a<5 && a>12){
+            System.out.println("Good morning!!");
+            } 
+            else
+            System.out.println("not a morning..");
+    }
+}
